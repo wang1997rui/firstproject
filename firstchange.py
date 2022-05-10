38d07dc68766d84676a23d0b2f496f3225bf06f0
wang1997rui <wr970505150@qq.com>
2022-05-10 23:17:20 +0800

    wnagwangsecondchanges

--- firstchange.py
+++ firstchange.py
@@ -1,3 +1,23 @@
 print('Hello World!')
 print("wangwangbranch")
 
+
+def find_threedigit(arr, target):
+    arr.sort()
+    result = []
+    for i in range(len(arr) - 1):
+        if arr[0] > target:
+            return -1
+
+        for j in range(i + 1, len(arr)):
+            temp = arr[i] + arr[j]
+            if temp <= target and ((target - temp) in arr[j::]):
+                print(result.append([arr[i], arr[j], target - temp]))
+
+    return result
+
+
+if __name__ == "__main__":
+    arr = list(map(int, input().split()))
+    target = int(input())  # 测试输入的时候，写的是12，下面的结果为12 的结果
+    print(find_threedigit(arr, target))
